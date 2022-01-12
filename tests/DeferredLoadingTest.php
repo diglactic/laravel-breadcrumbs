473530df3fe859d9a7fa72bb663cf4ba53a8b80a
freek <freek@spatie.be>
2022-01-12 18:53:29 +0100

    wip

--- tests/DeferredLoadingTest.php
+++ tests/DeferredLoadingTest.php
@@ -34,7 +34,7 @@ class DeferredLoadingTest extends TestCase
     }
 }
 
-class DisabledConsoleKernel extends \Orchestra\Testbench\Console\Kernel
+class DisabledConsoleKernel extends \Illuminate\Foundation\Console\Kernel
 {
     public function bootstrap()
     {
