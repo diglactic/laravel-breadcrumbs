5b33edb84fa3f7257cb66b5a14a80fc45951c4bb
Sheng-Liang Slogar <sheng@diglactic.com>
2023-04-17 11:34:32 -0500

    Update typings

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -10,16 +10,16 @@ use Illuminate\Support\Facades\Facade;
  * @method static void for(string $name, callable $callback)
  * @method static void before(callable $callback)
  * @method static void after(callable $callback)
- * @method static bool exists(string $name = NULL)
- * @method static \Illuminate\Support\Collection generate(string $name = NULL, ...$params)
- * @method static \Illuminate\Contracts\View\View view(string $view, string $name = NULL, ...$params)
- * @method static \Illuminate\Contracts\View\View render(string $name = NULL, ...$params)
+ * @method static bool exists(?string $name = null)
+ * @method static \Illuminate\Support\Collection generate(?string $name = null, ...$params)
+ * @method static \Illuminate\Contracts\View\View view(string $view, ?string $name = null, ...$params)
+ * @method static \Illuminate\Contracts\View\View render(?string $name = null, ...$params)
  * @method static object|null current()
  * @method static array getCurrentRoute()
  * @method static void setCurrentRoute(string $name, ...$params)
  * @method static void clearCurrentRoute()
  * @mixin \Illuminate\Support\Traits\Macroable
- * @see Manager
+ * @see \Diglactic\Breadcrumbs\Manager
  */
 class Breadcrumbs extends Facade
 {
