016d8c6393491ced2361860d3ecb2580e82f9f55
ahmed saad <alfker3@gmail.com>
2024-02-04 15:12:38 +0200

    Add SEO BreadcrumbList schema

--- resources/views/bootstrap4.blade.php
+++ resources/views/bootstrap4.blade.php
@@ -1,14 +1,15 @@
 @unless ($breadcrumbs->isEmpty())
     <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
+        <ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+            @php $i = 1; @endphp
             @foreach ($breadcrumbs as $breadcrumb)
 
                 @if ($breadcrumb->url && !$loop->last)
-                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                    <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"><a itemprop="item" href="{{ $breadcrumb->url }}"><span itemprop="name">{{ $breadcrumb->title }}</span></a></li>
                 @else
                     <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
                 @endif
-
+                @php $i++; @endphp
             @endforeach
         </ol>
     </nav>
--- resources/views/bootstrap5.blade.php
+++ resources/views/bootstrap5.blade.php
@@ -1,14 +1,15 @@
 @unless ($breadcrumbs->isEmpty())
     <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
+        <ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+            @php $i = 1; @endphp
             @foreach ($breadcrumbs as $breadcrumb)
 
                 @if ($breadcrumb->url && !$loop->last)
-                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                    <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"><a itemprop="item" href="{{ $breadcrumb->url }}"><span itemprop="name">{{ $breadcrumb->title }}</span></a><meta itemprop="position" content="{{ $i }}" /></li>
                 @else
-                    <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb->title }}</li>
+                    <li class="breadcrumb-item active" aria-current="page"><span itemprop="name">{{ $breadcrumb->title }}</span><meta itemprop="position" content="{{ $i }}" /></li>
                 @endif
-
+                @php $i++; @endphp
             @endforeach
         </ol>
     </nav>
