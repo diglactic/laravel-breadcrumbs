d5f1ff0f02c033c052a057c5745628fe87a44f94
Ahmed Saad <alfker3@gmail.com>
2024-02-04 14:53:58 +0200

    Update bootstrap5.blade.php
    Add SEO BreadcrumbList schema

--- resources/views/bootstrap5.blade.php
+++ resources/views/bootstrap5.blade.php
@@ -1,14 +1,14 @@
 @unless ($breadcrumbs->isEmpty())
     <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
+        <ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+            @php $i = 1; @endphp
             @foreach ($breadcrumbs as $breadcrumb)
-
                 @if ($breadcrumb->url && !$loop->last)
-                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                    <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"><a itemprop="item" href="{{ $breadcrumb->url }}"><span itemprop="name">{{ $breadcrumb->title }}</span></a><meta itemprop="position" content="{{ $breadcrumb->index }}" /></li>
                 @else
-                    <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb->title }}</li>
+                    <li class="breadcrumb-item active" aria-current="page" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"><span itemprop="name">{{ $breadcrumb->title }}</span><meta itemprop="position" content="{{ $breadcrumb->index }}" /></li>
                 @endif
-
+                @php $i++; @endphp
             @endforeach
         </ol>
     </nav>
